1a820379ee483fe9055fe9020a257e5f12755c2c
mdholder74 <mdholder74@gmail.com>
2025-01-01 14:40:43 -0500

    Completed Hero sections and add skills png

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+    menu.classList.toggle("open");// This will toggle the class open on the menu when the hamburger icon is clicked
+    icon.classList.toggle("open"); // This will toggle the class open on the hamburger icon when it is clicked
 }
